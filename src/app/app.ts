26a1c5bba8381486462cd1b9fe2336c55a906926
artemgassan <artemgassan@ya.ru>
2025-07-14 13:32:52 +0300

    feat: add layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Layout } from './components/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [TuiRoot, Layout],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
