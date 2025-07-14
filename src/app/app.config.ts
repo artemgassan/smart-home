2af85f98c33237c534dfd042c9a905d7399c6ec8
artemgassan <artemgassan@ya.ru>
2025-07-14 11:39:09 +0300

    refactor: fixed eslint warnings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
-import { provideEventPlugins } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import type { ApplicationConfig } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -8,9 +8,9 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideAnimations(),
-        provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-        provideEventPlugins()
-    ],
+    provideEventPlugins(),
+  ],
 };
