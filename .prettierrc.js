7e1379db17f5340e1605966856dd5cfa9eab979b
artemgassan <artemgassan@ya.ru>
2025-07-16 10:35:56 +0300

    refactor: change prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 120,
+};
+
+export default config;
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,8 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, smart-home',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, smart-home');
   });
 });
