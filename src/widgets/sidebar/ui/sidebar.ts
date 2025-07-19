08f9ba4f1d560a2b7ec027059641ebb379c6e3d9
artemgassan <artemgassan@ya.ru>
2025-07-19 16:39:42 +0300

    feat: added check for lyaout

--- src/widgets/sidebar/ui/sidebar.ts
+++ src/widgets/sidebar/ui/sidebar.ts
@@ -4,12 +4,21 @@ import { SidebarMenu } from '@/features/sidebar-menu';
 import { SidebarHeader } from '@/features/sidebar-header';
 import { SidebarFooter } from '@/features/sidebar-footer';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { TuiAsideComponent, tuiLayoutIconsProvider, TuiMainComponent } from '@taiga-ui/layout';
+import { TuiAsideComponent, tuiLayoutIconsProvider, TuiMainComponent, TuiNavigation } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [FormsModule, SidebarMenu, TuiAsideComponent, SidebarHeader, SidebarFooter, Dashboard, TuiMainComponent],
+  imports: [
+    FormsModule,
+    SidebarMenu,
+    TuiAsideComponent,
+    SidebarHeader,
+    SidebarFooter,
+    Dashboard,
+    TuiMainComponent,
+    TuiNavigation,
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   providers: [tuiLayoutIconsProvider({ grid: '@tui.align-justify' })],
