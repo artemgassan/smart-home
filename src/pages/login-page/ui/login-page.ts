8ada2c392a7f801781ee7021e6059796358d4eda
artemgassan <artemgassan@ya.ru>
2025-07-24 21:50:21 +0300

    feat: login page added

---
+++ src/pages/login-page/ui/login-page.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [],
+  standalone: true,
+  templateUrl: './login-page.html',
+  styleUrl: './login-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPage {
+
+}
