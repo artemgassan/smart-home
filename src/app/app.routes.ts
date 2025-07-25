6ff9ff6fd7a13bca51c0b384f82088e22f560f11
artemgassan <artemgassan@ya.ru>
2025-07-26 00:35:56 +0300

    feat: redirect to the main page implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ import { DashboardPage } from '@/pages/dashboard-page';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
     component: DashboardPage,
   },
   {
