9a5d9ac4766bdbf2923fb3896397ed5a3bf62dc7
artemgassan <artemgassan@ya.ru>
2025-07-28 18:46:40 +0300

    fix: fixed imports

--- public/data/mock-response.ts
+++ public/data/mock-response.ts
@@ -1,4 +1,4 @@
 import response from './mock-data.json';
-import type { DashboardType } from '@/app/components/dashboard/dashboard.model';
+import { DashboardType } from '@/app/interfaces/tabs.interface';
 
 export const responseData: DashboardType = JSON.parse(JSON.stringify(response));
