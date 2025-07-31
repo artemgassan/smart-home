284461c23bd8d2f73328951977fe4395d46b1e9b
artemgassan <artemgassan@ya.ru>
2025-07-31 21:49:26 +0300

    feat: added api request for dashboard

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
 import { TabsSwitcher } from '@/app/interfaces/tabs.interface';
 import { CardList } from '@/app/components/card-list/card-list';
+import type { DashboardType } from '@/app/interfaces/tabs.interface';
+import { DashboardsService } from '@/app/services/dashboards.service';
 import { TabSwitcher } from '@/app/components/tab-switcher/tab-switcher';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { TabToCardsPipe } from '@/app/components/dashboard/pipes/tab-to-cards.pipe';
 
 @Component({
@@ -13,5 +15,13 @@ import { TabToCardsPipe } from '@/app/components/dashboard/pipes/tab-to-cards.pi
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {
+  public dashboard = signal<DashboardType | null>(null);
   public activeTab = signal<TabsSwitcher>(TabsSwitcher.overview);
+  private api = inject(DashboardsService);
+
+  constructor() {
+    this.api.getDashboard().subscribe({
+      next: (data) => this.dashboard.set(data),
+    });
+  }
 }
--- src/app/services/dashboards.service.ts
+++ src/app/services/dashboards.service.ts
@@ -1,17 +1,28 @@
 import type { Observable } from 'rxjs';
 import { catchError, throwError } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import type { DashboardResponse } from '@/app/interfaces/tabs.interface';
+import type { DashboardResponse, DashboardType } from '@/app/interfaces/tabs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardsService {
   private http = inject(HttpClient);
+  private route = inject(ActivatedRoute);
 
   public getDashboards(): Observable<DashboardResponse[]> {
     return this.http.get<DashboardResponse[]>('/dashboards').pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      }),
+    );
+  }
+
+  public getDashboard(): Observable<DashboardType> {
+    const dashboardId = this.route.snapshot.paramMap.get('dashboardId') ?? '';
+    return this.http.get<DashboardType>(`/dashboards/${dashboardId}`).pipe(
       catchError((error) => {
         return throwError(() => error);
       }),
