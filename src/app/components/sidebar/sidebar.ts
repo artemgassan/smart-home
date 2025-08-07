84c78977e26880c34ce02ff177d2c55190fcc76f
artemgassan <artemgassan@ya.ru>
2025-08-07 20:05:21 +0300

    fix: fixed sidebar display on small resolutions

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,27 +1,27 @@
-import type { OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { SidebarMenu } from '@/app/components/sidebar/sidebar-menu/sidebar-menu';
-import { SidebarHeader } from '@/app/components/sidebar/sidebar-header/sidebar-header';
-import { SidebarFooter } from '@/app/components/sidebar/sidebar-footer/sidebar-footer';
 import {
   TuiNavigation,
   TuiAsideComponent,
   TuiMainComponent,
   tuiLayoutIconsProvider,
 } from '@taiga-ui/layout';
+import type { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { SidebarMenu } from '@/app/components/sidebar/sidebar-menu/sidebar-menu';
+import { SidebarHeader } from '@/app/components/sidebar/sidebar-header/sidebar-header';
+import { SidebarFooter } from '@/app/components/sidebar/sidebar-footer/sidebar-footer';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    FormsModule,
     SidebarMenu,
-    TuiAsideComponent,
+    FormsModule,
     SidebarHeader,
     SidebarFooter,
-    TuiMainComponent,
     TuiNavigation,
+    TuiMainComponent,
+    TuiAsideComponent,
   ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
@@ -30,7 +30,9 @@ import {
 })
 export class Sidebar implements OnInit {
   private static readonly widthSidebarCloses = 768;
+  private static readonly minSizeSidebarOpen = 580;
   protected expanded = signal(true);
+  protected showHeader = signal(true);
   private resizeListener = this.updateExpandedState.bind(this);
 
   public ngOnInit(): void {
@@ -44,6 +46,8 @@ export class Sidebar implements OnInit {
 
   private updateExpandedState(): void {
     const isMobile = window.innerWidth < Sidebar.widthSidebarCloses;
+    const shouldShowHeader = window.innerWidth >= Sidebar.minSizeSidebarOpen;
     this.expanded.set(!isMobile);
+    this.showHeader.set(shouldShowHeader);
   }
 }
