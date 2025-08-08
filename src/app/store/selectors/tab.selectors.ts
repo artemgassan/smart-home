b5e774513194432b60cc3c5c2043238028f3adbc
artemgassan <artemgassan@ya.ru>
2025-08-08 21:46:18 +0300

    refactor: change state types

--- src/app/store/selectors/tab.selectors.ts
+++ src/app/store/selectors/tab.selectors.ts
@@ -1,5 +1,5 @@
-import type { AppStateType } from '@/app/store/states/app.state';
 import { createSelector } from '@ngrx/store';
+import type { AppStateType } from '@/app/store/states/app.state';
 import type { TabStateType } from '@/app/store/states/tab.state';
 
 const selectUsers = (state: AppStateType): TabStateType => state.tabs;
--- src/app/store/states/tab.state.ts
+++ src/app/store/states/tab.state.ts
@@ -2,10 +2,10 @@ import type { TabType } from '@/app/interfaces/tabs.interface';
 
 export type TabStateType = {
   tabs?: TabType[];
-  selectedTab?: TabType;
+  selectedTab: TabType | null;
 };
 
 export const initialTabState: TabStateType = {
-  tabs: undefined,
-  selectedTab: undefined,
+  tabs: [],
+  selectedTab: null,
 };
