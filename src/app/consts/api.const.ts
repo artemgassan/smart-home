463f1fd964634b40a611c689602f09de50dbfb9b
artemgassan <artemgassan@ya.ru>
2025-08-24 17:07:40 +0300

    feat: changed the default tracking strategy

--- src/app/consts/api.const.ts
+++ src/app/consts/api.const.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://smart-ui-json-server-production.up.railway.app';
+export const BASE_URL = 'http://localhost:3004';
+// export const BASE_URL = 'https://smart-ui-json-server-production.up.railway.app';
