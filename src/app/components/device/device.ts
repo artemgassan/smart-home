94c5b167bb9f7687f1c0474e3349ec4d5c65b9d2
artemgassan <artemgassan@ya.ru>
2025-08-25 00:34:53 +0300

    feat: updating card status on the server

--- src/app/components/device/device.ts
+++ src/app/components/device/device.ts
@@ -1,9 +1,9 @@
-import { Store } from '@ngrx/store';
 import { TuiIcon } from '@taiga-ui/core';
 import { NgClass } from '@angular/common';
 import { TuiHeader } from '@taiga-ui/layout';
 import { TuiBlock, TuiSwitch } from '@taiga-ui/kit';
 import { EntityIconPipe } from '@/app/pipes/entity-icon.pipe';
+import { DevicesService } from '@/app/services/devices.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import type { DeviceItemType } from '@/app/interfaces/cards.interface';
 import { input, model, inject, computed, Component, ChangeDetectionStrategy } from '@angular/core';
@@ -28,11 +28,12 @@ export class Device {
   public item = model.required<DeviceItemType>();
   public state = computed<boolean>(() => this.item().state);
   public size = input.required<string>();
-  private readonly store = inject(Store);
+  private readonly api = inject(DevicesService);
   private readonly id = computed<string>(() => this.item().id);
 
   public toggleDeviceState(state: boolean): void {
     this.item.update((item) => ({ ...item, state }));
+    this.api.toggleDeviceState(this.id(), this.state()).subscribe();
   }
 
   protected getTitle(): string {
