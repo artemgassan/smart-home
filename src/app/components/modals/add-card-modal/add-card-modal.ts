44e40ec7ca71279b151840fcb483f7e340431256
artemgassan <artemgassan@ya.ru>
2025-08-27 15:50:42 +0300

    feat: initialized modal windows for adding cards

---
+++ src/app/components/modals/add-card-modal/add-card-modal.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-card-modal',
+  imports: [],
+  templateUrl: './add-card-modal.html',
+  styleUrl: './add-card-modal.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddCardModal {
+
+}
---
+++ src/app/components/modals/add-tab-modal/add-tab-modal.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tab-modal',
+  imports: [],
+  templateUrl: './add-tab-modal.html',
+  styleUrl: './add-tab-modal.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddTabModal {
+
+}
