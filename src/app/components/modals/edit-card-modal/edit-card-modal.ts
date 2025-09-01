8bc8f00e3f3ced3d98278d4c50e7e9dc3aca8804
artemgassan <artemgassan@ya.ru>
2025-09-02 02:25:34 +0300

    fix: fixed selector logic

--- src/app/components/modals/edit-card-modal/edit-card-modal.ts
+++ src/app/components/modals/edit-card-modal/edit-card-modal.ts
@@ -6,20 +6,21 @@ import { TuiIcon, TuiTitle } from '@taiga-ui/core';
 import type { TuiDialogContext } from '@taiga-ui/core';
 import { EntityIconPipe } from '@/app/pipes/entity-icon.pipe';
 import { POLYMORPHEUS_CONTEXT } from '@taiga-ui/polymorpheus';
-import { SensorPipe } from '@/app/components/sensor/pipes/sensor.pipe';
+import { selectRouteTabId } from '@/app/store/selectors/router.selectors';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { selectCardEntities } from '@/app/store/selectors/dashboard.selectors';
 
 @Component({
   selector: 'app-edit-card-modal',
-  imports: [TuiHeader, TuiTitle, TuiButton, EntityIconPipe, SensorPipe, TuiBlock, TuiIcon],
+  imports: [TuiHeader, TuiTitle, TuiButton, EntityIconPipe, TuiBlock, TuiIcon],
   templateUrl: './edit-card-modal.html',
   styleUrl: './edit-card-modal.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditCardModal {
   protected readonly store = inject(Store);
-  protected readonly entities = this.store.selectSignal(selectCardEntities);
+  protected readonly tabId = this.store.selectSignal(selectRouteTabId);
+  protected readonly entities = this.store.selectSignal(selectCardEntities(this.tabId()));
   private readonly context = inject<TuiDialogContext>(POLYMORPHEUS_CONTEXT);
 
   protected onCanselClick(): void {
--- src/app/store/selectors/dashboard.selectors.ts
+++ src/app/store/selectors/dashboard.selectors.ts
@@ -1,4 +1,6 @@
 import { createSelector } from '@ngrx/store';
+import type { MemoizedSelector } from '@ngrx/store';
+import type { ItemType } from '@/app/interfaces/cards.interface';
 import type { AppStateType } from '@/app/store/states/app.state';
 import type { DashboardStateType } from '@/app/store/states/dashboard.state';
 
@@ -24,7 +26,10 @@ export const selectDashboardId = createSelector(
   (state: DashboardStateType) => state.dashboardId,
 );
 
-export const selectCardEntities = createSelector(
-  selectDashboard,
-  (state: DashboardStateType) => state.viewData?.tabs[0]?.cards[0]?.items,
-);
+export const selectCardEntities = (
+  tabId: string,
+): MemoizedSelector<AppStateType, ItemType[] | undefined> =>
+  createSelector(
+    selectDashboard,
+    (state: DashboardStateType) => state.viewData?.tabs[Number(tabId)]?.cards[0]?.items,
+  );
