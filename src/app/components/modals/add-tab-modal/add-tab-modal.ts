2481739cf95ae00a85fc617040a7dc4d7d84aa88
artemgassan <artemgassan@ya.ru>
2025-10-19 14:36:07 +0300

    feat: migrated to yarn

--- src/app/components/modals/add-tab-modal/add-tab-modal.ts
+++ src/app/components/modals/add-tab-modal/add-tab-modal.ts
@@ -5,8 +5,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './add-tab-modal.html',
   styleUrl: './add-tab-modal.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddTabModal {
-
-}
+export class AddTabModal {}
